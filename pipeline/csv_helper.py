207983320763e1a87372ea41aaaf1779b7f9a501
Vitaliy Dymna <vitaliy.dymna@xcentium.com>
2019-09-13 11:02:37 -0500

    Added small CsvHelper class.

---
+++ pipeline/csv_helper.py
@@ -0,0 +1,16 @@
+import csv
+import itertools
+
+
+class CsvHelper:
+    """Small helper class to work with CSV files."""
+
+    def build_csv_file(self, rows, file, header=None):
+        """Write to data to a CSV file based on a list of tuples.
+           Header is optional but of provided must have proper column sequence."""
+        if header:
+            rows = itertools.chain([header], rows)
+        writer = csv.writer(file, delimiter=',')
+        writer.writerows(rows)
+        file.seek(0)
+        return file
